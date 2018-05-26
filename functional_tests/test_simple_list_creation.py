ce8f021fd96cde8cf45c545801942b1bfc0d0a56
paperworker-hsg <paperworker.hsg@gmail.com>
2018-05-26 16:44:31 +0900

    FT를 개별 파일로 분할

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,65 +1,12 @@
-# functional_tests/tests.py
+# functional_tests/test_simpelist_creation.py
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-
-import time
-import os
-
-MAX_WAIT = 10
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                    time.sleep(0.5)
-
-    def test_layout_and_styling(self):
-        # 에디스는 메인 페이지를 방문한다
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # 그녀는 입력 상자가 가운데 배치된 것을 본다
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2,
-        512,
-        delta=10
-        )
-
-        # 그녀는 새로운 리스트를 시작하고 입력 상자가
-        # 가운데 배치된 것을 확인한다
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2,
-        512,
-        delta=10
-        )
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
@@ -100,7 +47,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해준다
         # 이때 URL에 대한 설명도 함께 제공된다
-        self.fail('Finish the test!')
 
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
 
